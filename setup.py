37a65cc3547a0855764d7c7ed6eb064f22ea2bfe
Alex Davydow <alexey@synthesi.ai>
2021-02-25 19:53:20 +0300

    Formating

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(
     version=1.0,
     python_requires=">=3.6.0",
     license="MIT",
-    author='Synthesis AI',
+    author="Synthesis AI",
     install_requires=[
         "tiffile>=2018.10.18",
         "opencv-python>=4.5.1.48",
         "imagecodecs>=2021.1.28",
-        "numpy>=1.20.1"
+        "numpy>=1.20.1",
     ],
     include_package_data=True,
     description="Class to access datasets by FaceAPI datasets from Synthesis AI",
-    packages=find_packages()
+    packages=find_packages(),
 )

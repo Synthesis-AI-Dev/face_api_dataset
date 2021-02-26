8e8cfaacc178c1487f13539a3c3dce44237f25c9
Alex Davydow <alexey@synthesi.ai>
2021-02-26 15:54:42 +0300

    Minor version bump

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Synthesis AI"
 author = "Synthesis AI"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0"
+release = "1.0.1"
 
 
 # -- General configuration ---------------------------------------------------
